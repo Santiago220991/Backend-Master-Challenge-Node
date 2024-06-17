24ee26f9044b20cfa26dd938d8242cb8781c6e8e
Alexander Santiago Cárdenas <alexander.cardenas10@outlook.com>
2024-06-17 09:24:27 -0500

    Update baseCtrl.js

--- controllers/baseCtrl.js
+++ controllers/baseCtrl.js
@@ -1,11 +1,6 @@
 import path from 'path'
 
 export default {
-  /**
-   * Returns the base HTML page when the base route is hit.
-   * @param  req
-   * @param  res
-   */
   basePage: (req, res) => {
     res.status(200).sendFile(path.resolve(__dirname, '../static/base.html'))
   }
